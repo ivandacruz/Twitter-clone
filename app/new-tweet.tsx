6e2eeeab7c55bbba8ae3188ca7651ef67a6513a6
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2023-10-09 09:12:35 -0300

    add new tweet page

--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,4 +1,6 @@
-import { View, StyleSheet, Text, Image, TextInput } from 'react-native';
+import { Link, useNavigation, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { View, StyleSheet, Text, Image, TextInput, Pressable, SafeAreaView } from 'react-native';
 
 const user = {
     id: 'u1',
@@ -9,27 +11,76 @@ const user = {
 };
 
 export default function NewTweet() {
+    const [text, setText] = useState("");
+    const navigation = useRouter();
+
+    const onTweetPress = () => {
+        console.warn('Positing the tweet: ', text);
+        
+        setText('');
+        navigation.back();
+        // navigation.goBack();
+    };
+
     return (
-        <View style={styles.container}>
-            <View style={styles.inputContainer}>
-                <Image src={user.image} style={styles.image} />
-                <TextInput 
-                    placeholder="What's happening?"
-                    multiline
-                    numberOfLines={5} 
-                    style={{ flex: 1 }}
-                />
+        <SafeAreaView style={styles.body}>
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Link href="../" style={{ fontSize: 18 }}>
+                        Cancel
+                    </Link>
+                    <Pressable onPress={onTweetPress} style={styles.button}>
+                        <Text style={styles.buttonText}>Tweet</Text>
+                    </Pressable>
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Image src={user.image} style={styles.image} />
+                    <TextInput
+                        value={text}
+                        onChangeText={setText}
+                        placeholder="What's happening?"
+                        multiline
+                        numberOfLines={5}
+                        style={{ flex: 1 }}
+                    />
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
     );
 
 }
 
 const styles = StyleSheet.create({
+    body: {
+        flex: 1, 
+        justifyContent: 'space-between',
+        backgroundColor: 'white', 
+        marginVertical: 30, 
+        // padding: 20,
+    },
     container: {
-        padding: 10,
-        backgroundColor: 'white',
+        paddingHorizontal: 10,
         flex: 1,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 10,
+        justifyContent: 'space-between',
+    },
+    button: {
+        backgroundColor: '#1C9BF0',
+        padding: 10,
+        paddingHorizontal: 20,
+        borderRadius: 50,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    inputContainer: {
+        flexDirection: 'row',
     },
     image: {
         width: 50,
@@ -37,7 +88,5 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         marginRight: 10,
     },
-    inputContainer: {
-        flexDirection: 'row',
-    },
+
 });
